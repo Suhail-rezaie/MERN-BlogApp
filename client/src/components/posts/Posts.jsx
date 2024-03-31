e905b2ab9214fd2e3e174a237d8fa50d69a80162
BroCode <79736671+Suhail-rezaie@users.noreply.github.com>
2024-04-01 01:50:20 +0500

    Update Posts.jsx

--- client/src/components/posts/Posts.jsx
+++ client/src/components/posts/Posts.jsx
@@ -4,9 +4,13 @@ import "./posts.css";
 export default function Posts({ posts }) {
   return (
     <div className="posts">
-      {posts.map((p) => (
-        <Post key={p.id} post={p} />
-      ))}
+      {Array.isArray(posts) ? (
+        posts.map((p) => (
+          <Post key={p.id} post={p} />
+        ))
+      ) : (
+        <p>No posts available</p>
+      )}
     </div>
   );
 }
