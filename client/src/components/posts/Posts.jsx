c19c2b832c9a38dddb6a33287b4b92d1f79ad86e
BroCode <79736671+Suhail-rezaie@users.noreply.github.com>
2024-04-01 01:51:42 +0500

    Update Posts.jsx

--- client/src/components/posts/Posts.jsx
+++ client/src/components/posts/Posts.jsx
@@ -2,15 +2,16 @@ import Post from "../post/Post";
 import "./posts.css";
 
 export default function Posts({ posts }) {
+  if (!Array.isArray(posts)) {
+    // If posts is not an array, return null or render an appropriate error message
+    return <p>No posts available</p>;
+  }
+
   return (
     <div className="posts">
-      {Array.isArray(posts) ? (
-        posts.map((p) => (
-          <Post key={p.id} post={p} />
-        ))
-      ) : (
-        <p>No posts available</p>
-      )}
+      {posts.map((p) => (
+        <Post key={p.id} post={p} />
+      ))}
     </div>
   );
 }
